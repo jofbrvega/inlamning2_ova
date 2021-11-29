9c5237a9c82e870e63ed90eca941634be41200f7
jofbrvega <92310409+jofbrvega@users.noreply.github.com>
2021-11-29 21:42:49 +0100

    Update

--- main.js
+++ main.js
@@ -5,29 +5,30 @@ const btnInsert = document.getElementById("btnInsert");
 const lsOutput = document.getElementById("lsOutput");
 const plus = document.getElementsByClassName("plus");
 const minus = document.getElementsByClassName("minus");
-const select = document.getElementsByClassName("select");
+const option = document.getElementsByClassName("select");
 
-// Funktionen för denna kommer längre ner
-const inkomstLista = [];
-const kostnadLista = [];
-
-// Function for the addSum and put it in localstorage + Balance div
 btnInsert.onclick = function (){
       const key = inpKey.value;
       const value = inpValue.value;
-      
-      let costSum = 0;
-      let incomeSum = 0;
 
-      if (key && value){
-            localStorage.setItem(key, value);
-            location.reload();
-      } 
-};
-for (let i = 0; i < localStorage.length; i++){
-      const key = localStorage.key(i);
-      const value = localStorage.getItem(key);
-
-      lsOutput.innerHTML += `${key}: ${value}<br />`;
+      // Calculator effect
+if (option.value === "+"){
+      lsOutput.innerHTML +=`${key}: ${value}<br />`;
+} else {
+      lsInput.innerHTML += `${key}: ${value}<br />`;
 }
 
+
+
+// Local storage
+//       if (key && value){
+//             localStorage.setItem(key, value);
+//             location.reload();
+// };
+//       for (let i = 0; i < localStorage.length; i++){
+//             const key = localStorage.key(i);
+//             const value = localStorage.getItem(key);
+
+//       lsOutput.innerHTML += `${key}: ${value}<br />`;
+// };
+}
