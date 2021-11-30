a05ed73a955715ca3be58641ecf16a0169e2085e
jofbrvega <92310409+jofbrvega@users.noreply.github.com>
2021-11-30 11:31:09 +0100

    Last part Javascript

--- main.js
+++ main.js
@@ -5,19 +5,32 @@ const btnInsert = document.getElementById("btnInsert");
 const lsOutput = document.getElementById("lsOutput");
 const plus = document.getElementsByClassName("plus");
 const minus = document.getElementsByClassName("minus");
-const option = document.getElementsByClassName("select");
 
+// Button function adding to divs
 btnInsert.onclick = function (){
       const key = inpKey.value;
       const value = inpValue.value;
+      const select = document.querySelector("select")
 
-      // Calculator effect
-if (option.value === "+"){
+// Add budget and expenses in divs
+if (select.value=="plus"){
       lsOutput.innerHTML +=`${key}: ${value}<br />`;
-} else {
+} else if (select.value =="minus"){
       lsInput.innerHTML += `${key}: ${value}<br />`;
+} else if (select.value == "choose") {
+      alert("Stop! You must choose an operator.");
+} 
+
+// Subtract expense from Income and inserts answer inside "Remaining"
+function showRemaining(){
+var div1 = value.parseFloat(document.getElementById('lsOutput').value);
+var div2 = value.parseFloat(document.getElementById('lsInput').value);
+
+if (div1>=div2){
+      document.getElementById('remaining').value = div1 - div2;
 }
 
+}
 
 
 // Local storage
