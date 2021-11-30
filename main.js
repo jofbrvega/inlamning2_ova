066a38c12af4114c2124838d3169677f7986516f
jofbrvega <92310409+jofbrvega@users.noreply.github.com>
2021-11-30 13:54:18 +0100

    Färdig javascript

--- main.js
+++ main.js
@@ -5,43 +5,53 @@ const btnInsert = document.getElementById("btnInsert");
 const lsOutput = document.getElementById("lsOutput");
 const plus = document.getElementsByClassName("plus");
 const minus = document.getElementsByClassName("minus");
+const remainingBalance = document.getElementById("remaining");
+const isOutputdiv = document.getElementById('outputDiv')
+let itemList = []
 
 // Button function adding to divs
 btnInsert.onclick = function (){
       const key = inpKey.value;
-      const value = inpValue.value;
+      const value = inValue.value;
       const select = document.querySelector("select")
 
 // Add budget and expenses in divs
 if (select.value=="plus"){
       lsOutput.innerHTML +=`${key}: ${value}<br />`;
+      isOutputdiv.textContent = value;
+      console.log(isOutputdiv)
 } else if (select.value =="minus"){
-      lsInput.innerHTML += `${key}: ${value}<br />`;
+      lsInput.innerHTML += `${key}: ${value}<br />`
+      itemList.push(value)
+      console.log(itemList)
+      totalExpense()
+      showBalance();
 } else if (select.value == "choose") {
       alert("Stop! You must choose an operator.");
 } 
 
 // Subtract expense from Income and inserts answer inside "Remaining"
-function showRemaining(){
-var div1 = value.parseFloat(document.getElementById('lsOutput').value);
-var div2 = value.parseFloat(document.getElementById('lsInput').value);
+     function  totalExpense() {
+            let total = 0
+          const  itemLists = itemList.map((i) => Number(i));
+            if (itemLists.length > 0) {
+            total = itemLists.reduce(function (acc) {
+            acc += Number(value);
+            return acc;
+            }, 0)
+            } 
+            console.log(total)
+            return total;
+      }
 
-if (div1>=div2){
-      document.getElementById('remaining').value = div1 - div2;
-}
+      
+      function showBalance() {
+            const expense = totalExpense();
+            const total = Number(isOutputdiv.textContent) - expense;
+            console.log(lsOutput.textContent)
+            console.log(expense)
+            remainingBalance.textContent = total;
 
-}
 
-
-// Local storage
-//       if (key && value){
-//             localStorage.setItem(key, value);
-//             location.reload();
-// };
-//       for (let i = 0; i < localStorage.length; i++){
-//             const key = localStorage.key(i);
-//             const value = localStorage.getItem(key);
-
-//       lsOutput.innerHTML += `${key}: ${value}<br />`;
-// };
+      };
 }
